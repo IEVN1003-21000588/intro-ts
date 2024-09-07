d92bc49d9546dde6406862ffbd2c2d3f7db93b78
Mariana Ponce <atilanoponcemariana18@gmail.com>
2024-09-06 19:28:57 -0600

    cuarto commit

---
+++ src/temas-ts/01-tiposDat.ts
@@ -0,0 +1,23 @@
+var num1=38
+let num2='29'
+const num3=10
+ 
+ 
+let sum=num1+num2+num3;
+ 
+let nombre:string
+let num:number
+let activo:boolean
+ 
+nombre= 'mario'
+num= 3
+activo=true
+ 
+let matricula:number|string|boolean
+ 
+matricula=21000374
+matricula='SDDI24'
+ 
+ 
+ 
+console.log('el número es', sum);
---
+++ src/temas-ts/02-interfaz.ts
@@ -0,0 +1,16 @@
+interface Alumno{
+    nombre:string;
+    edad:number;
+    email: string;
+    calificacion?:number;
+}
+
+
+
+
+const aumo={
+    nombre:'mario',
+    edad:23,
+    email:'mario@gmail.com',
+    
+}
